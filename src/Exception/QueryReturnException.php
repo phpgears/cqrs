90803dfe02163ca5d87ed41b1026b2108bc3a836
Julián Gutiérrez <juliangut@gmail.com>
2019-02-03 22:52:18 +0100

    common exceptions

---
+++ src/Exception/QueryReturnException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * cqrs (https://github.com/phpgears/cqrs).
+ * CQRS base.
+ *
+ * @license MIT
+ * @link https://github.com/phpgears/cqrs
+ * @author Julián Gutiérrez <juliangut@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Gears\CQRS\Exception;
+
+class QueryReturnException extends \RuntimeException
+{
+}
